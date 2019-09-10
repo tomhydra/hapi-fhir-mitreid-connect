c730b34efad9381d739f38a8f880db62c7ba8803
Mekonnen_T <mekonnen.thomas@gmx.de>
2019-09-10 17:19:22 +0200

    Remove servlet-context.xml import in FhirTesterConfig to avoid duplicates

--- src/main/java/ca/uhn/fhir/jpa/starter/FhirTesterConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/FhirTesterConfig.java
@@ -22,9 +22,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @Import(FhirTesterMvcConfig.class)
-@ImportResource("classpath:servlet-context.xml")
 @ComponentScan(
-		basePackages = {"ca.uhn.fhir.to, ca.uhn.fhir.jpa.starter.controllers, org.mitre.web"}
+		basePackages = {"ca.uhn.fhir.to, org.mitre.openid.connect.client, ca.uhn.fhir.jpa.starter.controllers"}
 )
 public class FhirTesterConfig {
 
